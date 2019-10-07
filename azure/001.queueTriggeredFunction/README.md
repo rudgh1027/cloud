632e990f63a46973e7aff3f56097de168f9f7dfc
gkim <rudgh1027@gmail.com>
2019-10-07 07:32:29 +0000

    commit 001 folder

--- azure/001.queueTriggeredFunction/README.md
+++ azure/001.queueTriggeredFunction/README.md
@@ -1,2 +1,22 @@
-# cloud
-Cloud Computing Practice
+#Overview
+##Topic
+Queue Triggered Service Bus Queue
+##architecture
+
+#Procedure
+##Editting Parameter Name
+Queue Triggered Service Bus Queue
+##Deploy Service Bus Queue
+Queue Triggered Service Bus Queue
+##Deploy Azure Function App
+Queue Triggered Service Bus Queue
+##Make Project
+Queue Triggered Service Bus Queue
+##Editting .cs Source
+Queue Triggered Service Bus Queue
+##Local Build
+Queue Triggered Service Bus Queue
+###Test_1
+Queue Triggered Service Bus Queue
+##Deploy To Azure Function
+Queue Triggered Service Bus Queue
---
+++ azure/001.queueTriggeredFunction/deployq.sh
@@ -0,0 +1,20 @@
+# Create a resource group
+resourcegroupName=`cat exported.dat | grep resourcegroupName | awk -F "=" '{printf $2}'`
+location=`cat exported.dat | grep location | awk -F "=" '{printf $2}'`
+namespaceName=`cat exported.dat | grep namespaceName | awk -F "=" '{printf $2}'`
+qname=`cat exported.dat | grep qname | awk -F "=" '{printf $2}'`
+echo qname > ./qname.dat
+
+# Create a resource group
+az group create --name $resourcegroupName --location $location
+
+# Create a Service Bus messaging namespace with a unique name
+az servicebus namespace create --resource-group $resourcegroupName --name $namespaceName --location $location
+
+# Create a Service Bus queue
+az servicebus queue create --resource-group $resourcegroupName --namespace-name $namespaceName --name $qname
+
+# Get the connection string for the namespace
+connectionString=$(az servicebus namespace authorization-rule keys list --resource-group $resourcegroupName --namespace-name $namespaceName --name RootManageSharedAccessKey --query primaryConnectionString --output tsv)
+
+echo $connectionString > ./conn.dat
