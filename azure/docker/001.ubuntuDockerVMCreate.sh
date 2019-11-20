c498d94fe5339fa0196d23aa78429308fd12f2b2
rudgh1027 <37323388+rudgh1027@users.noreply.github.com>
2019-11-20 11:07:34 +0900

    Update 001.ubuntuDockerVMCreate.sh

--- azure/docker/001.ubuntuDockerVMCreate.sh
+++ azure/docker/001.ubuntuDockerVMCreate.sh
@@ -1,5 +1,36 @@
-## need to update
+##############
+## RG CREATE ##
+##############
+loc=                ##### INSERT YOUR VALUE!!!!
+rgname=             ##### INSERT YOUR VALUE!!!!
 
-az vm extension set --name DockerExtension --publisher Microsoft.Azure.Extensions --resource-group rg-gkim --vm-name vm-gkim 
+az group create --location $loc --name $rgname
+
+##############
+## VM CREATE ##
+##############
+
+adminname=          ##### INSERT YOUR VALUE!!!!
+
+az vm create \
+  --resource-group $rgname \
+  --name $vmname \
+  --image UbuntuLTS \
+  --admin-username $adminname \
+  --generate-ssh-keys \
+  --vnet-name $vnetname \
+  --storage-account $storname
+
+####################
+## DOCKER EXTENSION ##
+####################
+
+az vm extension set --name DockerExtension --publisher Microsoft.Azure.Extensions --resource-group $rgname --vm-name $vmname
 
 
+###################
+## CONNECT TO VM ##
+###################
+
+pip=$(az vm list-ip-addresses --name $vmname | jq ".[].virtualMachine.network.publicIpAddresses[0].ipAddress" -r)
+ssh $adminname@$pip
